ffd7c0c6e254757ecde1ee435fc7f9e1446f502a
n.d.grebnev <n.d.grebnev@tinkoff.ru>
2021-08-24 16:09:12 +0300

    extra

--- app/src/main/java/com/example/siriusdi/ui/LoginViewModel.kt
+++ app/src/main/java/com/example/siriusdi/ui/LoginViewModel.kt
@@ -3,15 +3,16 @@ package com.example.siriusdi.ui
 import com.example.siriusdi.data.LoginUserData
 import com.example.siriusdi.di.scopes.ActivityScope
 import com.example.siriusdi.domain.UserRepository
+import dagger.Lazy
 import javax.inject.Inject
 
 /**
  * @author n.d.grebnev
  */
 @ActivityScope
-class LoginViewModel @Inject constructor(private val userRepository: UserRepository) {
+class LoginViewModel @Inject constructor(private val userRepository: Lazy<UserRepository>) {
 
     fun loginUser(username: String, password: String): LoginUserData {
-        return userRepository.loginUser(username, password)
+        return userRepository.get().loginUser(username, password)
     }
 }
